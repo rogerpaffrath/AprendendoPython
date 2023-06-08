0a266b1dc10ce7c13f594b6888b755c80b1a171d
Roger Paffrath <rogerpaffrath@gmail.com>
2023-06-08 13:42:44 -0300

    Aula #01 - O básico do básico

---
+++ Aula01.py
@@ -0,0 +1,15 @@
+def testIfGoodDog(score) :
+    """This function tests if a dog is good or not."""
+    if(score > 90) :
+        print("Rogerinho is a good dog!")
+    else :
+        print("Rogerinho is not a good dog :(")
+
+print("Hello, world!") # Print on screen
+input("Press ENTER to continue...")
+print("Congratulations!")
+print("Let's move on.")
+
+dog_score = 80
+
+testIfGoodDog(dog_score)
