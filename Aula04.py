827161fa78985cf8c3ee488cbfe2d0eaf50f2fbb
Roger Paffrath <rogerpaffrath@gmail.com>
2023-06-18 21:39:20 -0300

    Aula #04

---
+++ Aula04.py
@@ -0,0 +1,29 @@
+# LET'S EXPLORE DATA TYPE
+# LET'S START WITH INTEGERS
+
+# There is no limit to the size of an int
+huge_number = 165498131684961316549846513216749841635165462346238
+other_huge_number = 23214348327019486712087462108736210
+huge_sum = huge_number + other_huge_number
+print(huge_sum)
+
+# HOW ABOUT FLOATS
+fraction = 4.2
+also_fraction = 4.
+less_than_zero = .5
+exponential = 4.2e7
+exponential_neg = 4.2e-3
+
+print(exponential_neg)
+
+# STRINGS
+a_string = "This is a string.\n"
+other_string = 'This is also a string.'
+empty_string = ""
+quoted_string = "My teacher said 'Python is easy'."
+triple_quote_string = """Testing the triple
+quoted string. We can have
+multiple line, '' single quotes and "" double quotes in it."""
+scaped_string = "This is a double quoted(\") string with a double quote inside."
+
+print(a_string + other_string)
